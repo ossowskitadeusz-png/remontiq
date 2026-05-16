ea6765f0aaf3233921fdaa46ef916b5dacd77fad
Tadeusz Ossowski <ossowski.tadeusz@gmail.com>
2026-05-16 23:49:03 +0200

    Fix: Change initial state to DRAFT to satisfy tasks_state_check constraint

--- services/task_service.py
+++ services/task_service.py
@@ -24,7 +24,7 @@ class TaskService:
             "name": name,
             "description": initial_handshake + description,
             "kanban_status": "TODO",
-            "state": "TODO",
+            "state": "DRAFT",
             "commercial_status": "pending",
             "created_at": datetime.now().isoformat()
         }
